9be173fd11e1d752d27d1f4fec6dd59f3bf144ae
Mikaël BRIDAY <mikael.briday@univ-nantes.fr>
2020-10-05 17:57:26 +0200

    squelette lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,30 +1,30 @@
 #include "stm32f3xx.h"
 
-void wait()
-{
-	volatile int i=0;
-	for(i=0;i<200000;i++);
+void wait() {
+  volatile int i = 0;
+  for (i = 0; i < 2000000; i++)
+    ;
 }
 
-void setup()
-{
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN_Msk; //clock for GPIOB
-	__asm("nop");					//wait until GPIOB clock is Ok.
-	GPIOB->MODER = 1 << (3*2);	//PB3 output
+void setup() {
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN_Msk; // clock for GPIOB
+  __asm("nop");                          // wait until GPIO clock is Ok.
+
+  // PB0 output
+  GPIOB->MODER &= ~GPIO_MODER_MODER0_Msk;
+  GPIOB->MODER |=  GPIO_MODER_MODER0_0;
 }
 
 /* main function */
-int main(void)
-{
-	setup();
-	/* Infinite loop */
-	while (1)
-	{
-		/* Add application code here */
-		GPIOB->BSRR = 1<<3; 		//bit set
-		wait();
-		GPIOB->BSRR = 1<<(3+16); 	//bit reset
-		wait();
-	}
+int main(void) {
+  setup();
+  /* Infinite loop */
+  while (1) {
+    /* Add application code here */
+    GPIOB->ODR |= GPIO_ODR_0; // bit set
+    wait();
+    GPIOB->ODR &= ~GPIO_ODR_0; // bit reset
+    wait();
+  }
 }
 
