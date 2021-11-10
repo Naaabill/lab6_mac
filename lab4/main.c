dccb37cc555f2bc0c50b08265e81153754648956
Mikaël BRIDAY <mikael.briday@ec-nantes.fr>
2021-11-10 11:27:11 +0100

    [vscode] vscode integration for lab4 (test)

--- lab4/main.c
+++ lab4/main.c
@@ -1,16 +1,15 @@
 #include "stm32f3xx.h"
+#include "pinAccess.h"
 
 void wait()
 {
     volatile int i=0;
-    for(i=0;i<200000;i++);
+    for(i=0;i<2000000;i++);
 }
 
 void setup()
 {
-    RCC->AHBENR |= RCC_AHBENR_GPIOBEN_Msk; //clock for GPIOB
-    __asm("nop");					//wait until GPIOB clock is Ok.
-    GPIOB->MODER = 1 << (3*2);	//PB3 output
+    pinMode(GPIOB,3,OUTPUT);
 }
 
 /* main function */
@@ -20,10 +19,7 @@ int main(void)
     /* Infinite loop */
     while (1)
     {
-        /* Add application code here */
-        GPIOB->BSRR = 1<<3; 		//bit set
-        wait();
-        GPIOB->BSRR = 1<<(3+16); 	//bit reset
+        digitalToggle(GPIOB,3);
         wait();
     }
 }
